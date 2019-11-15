ed8202cf709ce13bcaf7f46a03dc0b50ae560975
Nick <nikolayvaklinov@yahoo.com>
2019-11-15 12:08:15 +0000

    adding the the main function plus some imports

--- Constructive_Algorithms(Arrays)/Flipping_the_Matrix.py
+++ Constructive_Algorithms(Arrays)/Flipping_the_Matrix.py
@@ -1,3 +1,10 @@
+import math
+import os
+import random
+import re
+import sys
+
+
 def flippingMatrix(matrix):
     s = 0
     k = len(matrix[0])
@@ -6,3 +13,22 @@ def flippingMatrix(matrix):
             s += max(matrix[i][j], matrix[i][k-j-1], matrix[k-i-1][j], matrix[k-i-1][k-j-1])
     return s
 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        n = int(input())
+
+        matrix = []
+
+        for _ in range(2*n):
+            matrix.append(list(map(int, input().rstrip().split())))
+
+        result = flippingMatrix(matrix)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
